b8bfac58fba975c4110f289c27a3a7c2b9eab29f
steven_han <han_steven@bah.com>
2026-08-15 11:27:47 -0400

    Initialize pages/ folder for streamlit app

---
+++ pages/__init__.py
@@ -0,0 +1,7 @@
+# Treat the pages as a package to be imported into the main app
+
+# Import the pages
+from . import artist_explorer
+from . import temporal_trends
+from . import genre_comparison
+from . import prediction
